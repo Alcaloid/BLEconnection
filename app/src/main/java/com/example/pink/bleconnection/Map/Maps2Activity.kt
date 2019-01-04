2160b1d4f0076e2e6ceb7b065c42df38dd151a1f
Alcaloid <apichai.1507@mail.kmutt.ac.th>
2019-01-04 10:31:37 +0700

    Add function calculate distance

--- app/src/main/java/com/example/pink/bleconnection/Map/Maps2Activity.kt
+++ app/src/main/java/com/example/pink/bleconnection/Map/Maps2Activity.kt
@@ -32,9 +32,9 @@ class Maps2Activity : AppCompatActivity(), OnMapReadyCallback {
     lateinit var mScanner : BluetoothLeScanner
     //0,1 is x,y and 3 is rssi and 4 is receive rssi
     private var beacon0Information : IntArray = intArrayOf(0,0,0,0)
-    private var beacon1Information : IntArray = intArrayOf(40,0,0,0)
-    private var beacon2Information : IntArray = intArrayOf(0,40,0,0)
-    private var beacon3Information : IntArray = intArrayOf(40,40,0,0)
+    private var beacon1Information : IntArray = intArrayOf(11,0,0,0)
+    private var beacon2Information : IntArray = intArrayOf(0,15,0,0)
+    private var beacon3Information : IntArray = intArrayOf(11,15,0,0)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -66,17 +66,17 @@ class Maps2Activity : AppCompatActivity(), OnMapReadyCallback {
         mMap.mapType = GoogleMap.MAP_TYPE_NONE
         val testingRoom = LatLngBounds(
                 LatLng(0.0, 0.0),
-                LatLng(40.0,40.0))
+                LatLng(11.0,15.0))
         val testingMap = GroundOverlayOptions()
                 .image(BitmapDescriptorFactory.fromResource(R.drawable.seniortesting))
                 .positionFromBounds(testingRoom)
         val location = LatLng(0.0,0.0)
-        val locationZoom = LatLng(20.0,20.0)
+        val locationZoom = LatLng(5.5,7.5)
         val cameraTraget = LatLngBounds(
-                LatLng(-0.01,-0.01), LatLng(40.0, 40.0))
+                LatLng(-0.01,-0.01), LatLng(20.0, 20.0))
         mMap.addGroundOverlay(testingMap)
         mMap.addMarker(MarkerOptions().position(location).title("Marker"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(locationZoom,3f))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(locationZoom,10f))
         mMap.setLatLngBoundsForCameraTarget(cameraTraget);
 
         startScanner()
@@ -100,14 +100,26 @@ class Maps2Activity : AppCompatActivity(), OnMapReadyCallback {
                 beacon3Information[2] = result.rssi
                 beacon3Information[3] = 1
             }
-            if (beacon0Information[3]+beacon1Information[3]+beacon2Information[3]+beacon3Information[3]>=3){
-                calLocation(beacon0Information[2],beacon1Information[2],beacon2Information[2])
-            }
+//            if (beacon0Information[3]+beacon1Information[3]+beacon2Information[3]+beacon3Information[3]>=3){
+//                calLocation(beacon0Information[2],beacon1Information[2],beacon2Information[2])
+//            }
         }
     }
-    fun calLocation(rssi1:Int,rssi2: Int,rssi3: Int){
-
-//        mMap.addMarker(MarkerOptions().position(}).title("Marker"))
+    private fun getDistance(rssi: Int,txPower:Int):Double{
+        //d = 10 ^ ((TxPower - RSSI) / (10 * n))
+        val n: Int = 2
+        val distance : Double = Math.pow(10.0,((txPower-rssi)/(10.0*n)))
+        return distance
+    }
+    fun calLocation(R1:Int,R2:Int,R3:Int){
+        val x1: Double = 1.0
+        val x2: Double = 1.0
+        val x3: Double = 1.0
+        val y3: Double = 1.0
+        val X : Double = ((R1*R1)-(R2*R2)+(x2*x2))/(2*x2)
+        val Y : Double = ((R1*R1)-(R3*R3)+(x3*x3)+(y3*y3)-(2*x3*x1))/(2*y3)
+        val myLocation : LatLng = LatLng(X,Y)
+        mMap.addMarker(MarkerOptions().position(myLocation).title("Marker"))
     }
     override fun onDestroy() {
         super.onDestroy()
