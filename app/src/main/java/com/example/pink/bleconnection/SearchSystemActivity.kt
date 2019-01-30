5b1a364483449214733ca0f109b5c3c2d4938d9f
Alcaloid <apichai.1507@mail.kmutt.ac.th>
2019-01-30 14:37:18 +0700

    Add searchitem (unfinish)

--- app/src/main/java/com/example/pink/bleconnection/SearchSystemActivity.kt
+++ app/src/main/java/com/example/pink/bleconnection/SearchSystemActivity.kt
@@ -2,21 +2,41 @@ package com.example.pink.bleconnection
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.SearchView
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_search_system.*
 
 class SearchSystemActivity : AppCompatActivity() {
-    val place : ArrayList<String> = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_system)
-        place.add("Allcama")
-        place.add("Altoma")
-        place.add("ABOCAMIN")
-        place.add("BioHaZra")
+        val itemPlaceList = arrayOf("Alcatiob","AlBoBara","Action","BioHaJun")
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemPlaceList)
+        mListView.setAdapter(adapter)
 
-        serachArea.queryHint = "Hint of xxx"
-        serachArea.setOnQueryTextFocusChangeListener { v, hasFocus ->  }
+        serachBackground.setOnClickListener {
+            mListView.visibility = View.INVISIBLE
+        }
+        searchBar.addTextChangeListener(object : TextWatcher{
+            override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+                mListView.visibility = View.VISIBLE
+            }
+            override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+                //SEARCH FILTER
+                adapter.getFilter().filter(charSequence)
+            }
+
+            override fun afterTextChanged(editable: Editable) {
+
+            }
+        })
+        searchBar.clearSuggestions()
+        mListView.setOnItemClickListener { parent, view, position, id ->
+            searchBar.text = itemPlaceList[position]
+        }
 
     }
 }
