68eb100f06850db53220e0949855527a4d1e76dc
Alcaloid <apichai.1507@mail.kmutt.ac.th>
2019-02-27 10:01:54 +0700

    Make layout queue(finish) and add library qr scan

--- app/src/main/java/com/example/pink/bleconnection/QueFragment.kt
+++ app/src/main/java/com/example/pink/bleconnection/QueFragment.kt
@@ -1,9 +1,11 @@
 package com.example.pink.bleconnection
 
+import android.Manifest
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.graphics.PointF
 import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -11,10 +13,19 @@ import android.support.v4.app.NotificationCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.pink.bleconnection.Model.CalculatorFunction
-import kotlinx.android.synthetic.main.fragment_que.view.*
+import android.widget.Toast
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
+import kotlinx.android.synthetic.main.fragment_que.*
 
 class QueFragment : Fragment(){
+    private var qrCodeReaderView: QRCodeReaderView? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_que, container, false)
         return view
@@ -22,15 +33,29 @@ class QueFragment : Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val calFuction : CalculatorFunction = CalculatorFunction()
-        calFuction.toast("On Start",view.context)
-        /*view.noti1.setOnClickListener {
-            showNotification("Testing1","Complete",view.context)
+        qrCodeReaderView = view.findViewById(R.id.qrdecoderview)
+        qrCodeReaderView?.setOnQRCodeReadListener { text, points -> toast("Text :"+text) }
+        qrCodeReaderView?.setQRDecodingEnabled(true)
+        button_que_camera.setOnClickListener {
+            Dexter.withActivity(activity)
+                    .withPermission(Manifest.permission.CAMERA)
+                    .withListener(object : PermissionListener{
+                        override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+                            layout_scan_qr.visibility = View.GONE
+                            relative_que_camera_open.visibility = View.VISIBLE
+                        }
+                        override fun onPermissionRationaleShouldBeShown(permission: PermissionRequest?, token: PermissionToken?) {}
+                        override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+                            toast("This canera use to get queuing")
+                        }
+                    }).check()
         }
-        view.noti2.setOnClickListener {
-            showNotification("Testing2","New Carry",view.context)
-        }*/
+        button_camera_closer.setOnClickListener {
+            layout_scan_qr.visibility = View.VISIBLE
+            relative_que_camera_open.visibility = View.GONE
+        }
     }
+
     fun showNotification(textTitle : String,textContent : String,context: Context){
         val notificationManager = activity?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val builder = NotificationCompat.Builder(context, "1")
@@ -38,7 +63,7 @@ class QueFragment : Fragment(){
             val importance = NotificationManager.IMPORTANCE_HIGH
             var mChannel = notificationManager.getNotificationChannel("1")
             if (mChannel == null) {
-                mChannel = NotificationChannel("1","Noti", importance)
+                mChannel = NotificationChannel("1",textTitle, importance)
                 mChannel.enableVibration(true)
                 mChannel.setVibrationPattern(longArrayOf(500, 1000, 500))
                 notificationManager.createNotificationChannel(mChannel)
@@ -60,5 +85,8 @@ class QueFragment : Fragment(){
         }
         val notification = builder.build()
         notificationManager.notify(1, notification)
+    }
+    fun toast(text : String){
+        Toast.makeText(context,text, Toast.LENGTH_SHORT).show()
     }
 }
